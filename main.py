7b9f4865d2cbe8a35229f523ab3a57f331ecf235
lucaspintos909 <lucaspintos909@gmail.com>
2023-02-18 12:57:10 -0300

    feat: Implementando configuracion de nginx al flujo

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # importing required modules
-import os, sys, argparse, shutil
+import os
+import sys
+import argparse
+import shutil
 from validators import domains_validator
-
+from scripts import write_nginx_configuration
 # create a parser object
 parser = argparse.ArgumentParser(description="NGINX proyect configurator.")
 
@@ -14,11 +17,18 @@ args = parser.parse_args()
 
 columns = shutil.get_terminal_size().columns
 
+domains = args.domains
+
 # Validations
-domain_verification = domains_validator(args.domains)
+domain_verification = domains_validator(domains)
 if not domain_verification["valid"]:
-  print(domain_verification["message"])
-  sys.exit()
+    print(domain_verification["message"])
+    sys.exit()
+
+nginx_configuration_result = write_nginx_configuration(domains)
+if not nginx_configuration_result["valid"]:
+    print(nginx_configuration_result["message"])
+    sys.exit()
 
 """ if not result["valid"]:
   print(f"**** Error: {result['message']}".center(columns))
