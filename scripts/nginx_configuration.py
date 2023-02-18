76a731a478a28d435afcbbd762ce784b74d6722d
lucaspintos909 <lucaspintos909@gmail.com>
2023-02-18 14:20:47 -0300

    feat: Agregando configuracion para dominios con redireccion

--- scripts/nginx_configuration.py
+++ scripts/nginx_configuration.py
@@ -1,27 +1,31 @@
 import os
-from .config_template import get_config_template, get_html_template
+from .config_template import get_config_template, get_html_template, get_config_redirection_template
 
 NGINX_PATH = "/etc/nginx"
 PROJECTS_PATH = "/var/www/html"
 
 
-def get_nginx_configuration(domains: list) -> str:
+def get_nginx_configuration(domains: list, redirection) -> str:
     www_domain = domains[0] if "www" in domains[0] else domains[1]
     non_www_domain = domains[0] if not "www" in domains[0] else domains[1]
-    configuration = get_config_template(www_domain, non_www_domain)
+
+    if redirection:
+        configuration = get_config_redirection_template(www_domain, non_www_domain, redirection)
+    else:
+        configuration = get_config_template(www_domain, non_www_domain)
+
     return configuration
 
 # TODO: Hacer validaciones de si los comandos se ejecutan de manera correcta
 
 
-def write_nginx_configuration(domains:  str) -> dict:
+def write_nginx_configuration(domains:  str, redirection=None) -> dict:
     errors = False
     # Obtengo dominio sin prefijo www
     non_www_domain = domains[0] if not "www" in domains[0] else domains[1]
 
-    # Genero configuraciones
-    nginx_conf_string = get_nginx_configuration(domains)
-    html_template = get_html_template(non_www_domain)
+    # Genero configuracion
+    nginx_conf_string = get_nginx_configuration(domains, redirection)
 
     # Creo archivo de configuracion de Nginx
     nginx_file_path = f"{NGINX_PATH}/sites-available/{non_www_domain}"
@@ -39,19 +43,22 @@ def write_nginx_configuration(domains:  str) -> dict:
     # Reinicio servicio de NGINX
     if ext_code == 0:
         os.system("systemctl restart nginx")
-    else: 
+    else:
         errors = True
 
-    # Verifico si existe el directorio del proyecto
-    # y si no existe lo creo
-    project_path = f"{PROJECTS_PATH}/{non_www_domain}"
-    if not os.path.exists(project_path):
-        os.mkdir(project_path)
+    if not redirection:
+        html_template = get_html_template(non_www_domain)
 
-    # Creo el index.html de prueba si no existe
-    if not os.path.exists(f"{project_path}/index.html"):
-        with open(f"{project_path}/index.html", "w") as html_file:
-            html_file.write(html_template)
+        # Verifico si existe el directorio del proyecto
+        # y si no existe lo creo
+        project_path = f"{PROJECTS_PATH}/{non_www_domain}"
+        if not os.path.exists(project_path):
+            os.mkdir(project_path)
+
+        # Creo el index.html de prueba si no existe
+        if not os.path.exists(f"{project_path}/index.html"):
+            with open(f"{project_path}/index.html", "w") as html_file:
+                html_file.write(html_template)
 
     result = {"message": "", "valid": not errors}
     return result
